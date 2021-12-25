adc8aa96154331ba3ba27b99eaf1313885d6c87f
derkach9 <93737093+derkach9@users.noreply.github.com>
2021-12-25 19:08:55 +0200

    Create new version (#8)

--- module-nodejs-0877e49e23b145ed1248208e3a7beeef5f41e334/configs/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    config: require('./config'),
-    constants: require('./constants'),
-    userRoles: require('./user-roles.enum'),
-    tokenTypeEnum: require('./token-type.enum'),
-    emailActionsEnum: require('./email-action.enum'),
-    actionTokenTypeEnum: require('./action-token-type.enum')
-};
--- module-nodejs-0877e49e23b145ed1248208e3a7beeef5f41e334/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    authRouter: require('./auth.router'),
-    userRouter: require('./user.router'),
-    carRouter: require('./car.router')
-};
